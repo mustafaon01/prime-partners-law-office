301f5103a462efdbc9554c7024c32e66bbefaff9
Mustafa Oncu <mustafaoncu@Mustafas-Mini.home>
2024-05-01 22:03:21 +0300

    initial

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from prime_blog.settings import *
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
